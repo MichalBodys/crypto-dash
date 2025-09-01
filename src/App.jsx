be01c4c032c7efe88cdb93b973c3596689bfceb9
MichalBodys <113517249+MichalBodys@users.noreply.github.com>
2025-09-01 20:28:56 +0200

    crypto card component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import CryptoCard from './components/CryptoCard'
 const API_URL = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
         if(!res.ok)throw new Error('Faiiled to fetch data');
         const data = await res.json();
         console.log(data)
+        setCoins(data)
       } catch (err){
         setError(err.message);
       }finally{
@@ -25,7 +27,15 @@ const App = () => {
   return (
     <div>
       <h1>Crypto dashboard 🚀</h1>
-
+      {loading && <p>Loading...</p>}
+      {error && <div className='error'>{error}</div>}
+      {!loading && !error && (
+        <main className='grid'>
+          {coins.map((coin) => (
+            <CryptoCard key={coin.id} coin={coin} />
+          ) )}
+        </main>
+      )}
     </div>
   )
 }
